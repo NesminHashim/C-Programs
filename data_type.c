a10b5953d9462c688c3e2caf46409cb6db60de16
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-07-09 17:30:00 +0530

    Add variables and print statements in data_type.c

---
+++ data_type.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNumber = 21;
+    float myWeight = 63.78;
+    char letter = 'N';
+    char name[] = "Nesmin Hashim";
+    
+    printf("%d\n", myNumber);
+    printf("%f\n", myWeight);
+    printf("%c\n", letter);
+    printf("%s\n", name);
+    
+    return 0;
+}
+  
