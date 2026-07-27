8705a754519b32f9f00fa114648e404ba6172098
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-07-27 15:09:42 +0530

    Add arithmetic operators example in C
    Used all the arithmetic operators (Addition, Subtraction, Multiplication, Division, Modulus, Increment and Decrement)

---
+++ Arithmatic operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 5;
+
+    printf("%d\n", num1 + num2);
+     printf("%d\n", num1 - num2);
+      printf("%d\n", num1 * num2);
+       printf("%d\n", num1 / num2);
+        printf("%d\n", num1 % num2);
+         printf("%d\n", ++num1);
+          printf("%d", --num1);
+
+    return 0;
+}
