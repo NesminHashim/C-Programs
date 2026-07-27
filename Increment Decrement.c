da42b2f5c9109a7b09aa700aeca7c190ff714107
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-07-27 15:31:25 +0530

    Implement increment and decrement functionality
    Imagine you are building a program to count how many people enter and leave a room.

---
+++ Increment Decrement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int personEnterRoom =0;
+    
+    personEnterRoom++;
+    personEnterRoom++;
+    personEnterRoom++;
+    personEnterRoom++;
+    
+    printf("%d\n", personEnterRoom);
+    
+    personEnterRoom--;
+    personEnterRoom--;
+    personEnterRoom--;
+    
+    printf("%d\n", personEnterRoom);
+    return 0;
+}
