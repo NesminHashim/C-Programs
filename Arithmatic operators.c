d84e3b6fa117e1fd009680bdc7e74eb42cc2d751
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-08-22 17:10:18 +0530

    Add unary plus and minus operations

--- Arithmatic operators.c
+++ Arithmatic operators.c
@@ -9,6 +9,8 @@ int main() {
     printf("%d\n", num1 * num2);
     printf("%d\n", num1 / num2);
     printf("%d\n", num1 % num2);
+    printf("%d\n", +num1);
+    printf("%d\n", -num1);
     printf("%d\n", ++num1);
     printf("%d", --num1);
 
