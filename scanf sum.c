5aa1cd3ea07fa115eb24258f52b99476498f52df
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-09-12 13:08:19 +0530

    Add scanf program to calculate sum of two numbers
    Initialize variables and calculate sum after user input.

---
+++ scanf sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    int sum = a + b;
+
+    print("Enter the numbers : %d");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
